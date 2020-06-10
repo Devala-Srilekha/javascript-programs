54ccb821b4e488b61628bf35744f486b2338dfcb
Devala-Srilekha <57786670+Devala-Srilekha@users.noreply.github.com>
2020-06-10 15:52:36 +0530

    Delete palindrome.js

--- palindrome.js
+++
@@ -1,6 +0,0 @@
-function palindrome(string)
-{
-    string=string.replace('/\W/g','').toLowerCase();
-    return (string == string.split('').reverse().join(''));
-}
-console.log(palindrome('malayalam'));
